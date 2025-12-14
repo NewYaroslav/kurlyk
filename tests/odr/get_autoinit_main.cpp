da562ad2c26b44f7f6f92dad2689afb846f32008
Anastasia Prokop <gpae98@gmail.com>
2025-12-14 16:38:31 +0300

    add odr tests and ci script

---
+++ tests/odr/get_autoinit_main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <kurlyk.hpp>
+
+extern "C" kurlyk::startup::AutoInitializer* get_auto_init_a();
+extern "C" kurlyk::startup::AutoInitializer* get_auto_init_b();
+
+int main() {
+    auto* a = get_auto_init_a();
+    auto* b = get_auto_init_b();
+
+    std::cout << "AutoInitializer A: " << (const void*)a
+              << " AutoInitializer B: " << (const void*)b << "\n";
+
+    if (a != b) {
+        std::cerr << "There are 2 different AutoInitializer instances!\n";
+        return 1;
+    }
+	
+	std::cout << "There's only AutoInitializer, singlton works correctly" << std::endl;
+    return 0;
+}
--- tests/odr/get_singletons_a.cpp
+++ tests/odr/get_singletons_a.cpp
@@ -4,3 +4,11 @@
 extern "C" kurlyk::core::NetworkWorker* get_net_worker_a() { 
 	return std::addressof(kurlyk::core::NetworkWorker::get_instance()); 
 }
+
+extern "C" kurlyk::HttpRequestManager* get_http_request_manager_a() { 
+	return std::addressof(kurlyk::HttpRequestManager::get_instance()); 
+}
+
+extern "C" kurlyk::WebSocketManager* get_websocket_manager_a() { 
+	return std::addressof(kurlyk::WebSocketManager::get_instance()); 
+}
