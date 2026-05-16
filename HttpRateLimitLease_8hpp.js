55d44a064cface1088127b10d68c45bc3fc497f0
NewYaroslav <NewYaroslav@users.noreply.github.com>
2026-05-16 07:51:24 +0000

    Publish generated documentation

--- HttpRateLimitLease_8hpp.js
+++
@@ -1,8 +0,0 @@
-var HttpRateLimitLease_8hpp =
-[
-    [ "kurlyk::HttpRateLimitLease", "classkurlyk_1_1HttpRateLimitLease.html", "classkurlyk_1_1HttpRateLimitLease" ],
-    [ "kurlyk::HttpRateLimitLease::LimitLock", "structkurlyk_1_1HttpRateLimitLease_1_1LimitLock.html", "structkurlyk_1_1HttpRateLimitLease_1_1LimitLock" ],
-    [ "kurlyk::HttpRateLimitLease::CooldownLock", "structkurlyk_1_1HttpRateLimitLease_1_1CooldownLock.html", "structkurlyk_1_1HttpRateLimitLease_1_1CooldownLock" ],
-    [ "_KURLYK_HTTP_RATE_LIMIT_LEASE_HPP_INCLUDED", "HttpRateLimitLease_8hpp.html#a0f3407bb6b7e12d3b3d3f876b104bd64", null ],
-    [ "kurlyk::HttpRateLimitLeasePtr", "namespacekurlyk.html#a59df2ed0f5514ecd36cb6eb79560170a", null ]
-];
--- search/variables_13.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['url_0',['url',['../classkurlyk_1_1HttpRequest.html#a56e04746cd86cb402533e1d3463f316e',1,'kurlyk::HttpRequest::url'],['../classkurlyk_1_1WebSocketConfig.html#a66f7eaa5b340b73684e35b7a4ae4102e',1,'kurlyk::WebSocketConfig::url']]],
-  ['use_1',['use',['../structcryptox_1_1ProxyConfig.html#a6989ba8750f29708690904a8166942b5',1,'cryptox::ProxyConfig']]],
-  ['use_5finterface_2',['use_interface',['../classkurlyk_1_1HttpRequest.html#a4d90fae6a6ee0887f84482572eb70c5f',1,'kurlyk::HttpRequest']]],
-  ['user_5fagent_3',['user_agent',['../classkurlyk_1_1HttpRequest.html#a11d0d33609eb065490d13f5d3c487d34',1,'kurlyk::HttpRequest::user_agent'],['../classkurlyk_1_1WebSocketConfig.html#a55048b506fde5020ca7da67fb064a13c',1,'kurlyk::WebSocketConfig::user_agent']]]
-];
