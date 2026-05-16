502dcb1481b5bdd71b163d43a6f85f6585ca6c30
Aster Seker <git.electroyar@gmail.com>
2026-05-16 11:07:30 +0300

    test(deps): prefer pinned Simple-Web-Server submodule

--- cmake/deps/fallbacks/load_simple_web_server.cmake
+++ cmake/deps/fallbacks/load_simple_web_server.cmake
@@ -4,6 +4,19 @@ function(load_simple_web_server target)
 		set(USE_STANDALONE_ASIO ${KURLYK_USE_STANDALONE_ASIO}
 		  CACHE BOOL "Synchronization for Simple-Web-Server" FORCE)
 	endif()
+
+	set(simple_web_server_submodule_dir "${CMAKE_CURRENT_LIST_DIR}/../../../external/Simple-Web-Server")
+
+	if(EXISTS "${simple_web_server_submodule_dir}/server_http.hpp")
+		message(STATUS "Simple-Web-Server: using pinned submodule")
+		if (NOT TARGET simple_web_server)
+			add_library(simple_web_server INTERFACE)
+			target_include_directories(simple_web_server INTERFACE "${simple_web_server_submodule_dir}")
+		endif()
+		target_link_libraries(${target} PRIVATE simple_web_server)
+		return()
+	endif()
+
 	include(FetchContent)
 	FetchContent_Declare(simple_web_server
 		GIT_REPOSITORY https://gitlab.com/eidheim/Simple-Web-Server.git
